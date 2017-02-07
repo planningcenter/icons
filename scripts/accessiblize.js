c7e7f525613beff7641d2ade13238c6c34d012d5
Michael Chan <mijoch@gmail.com>
2017-02-07 14:06:21 -0800

    script: add 'accessiblize'
    Adds a script for destructively adding accessibility to the SVGs.
    
    For this pass, it only adds "aria-labelledby".

---
+++ scripts/accessiblize.js
@@ -0,0 +1,29 @@
+const fs = require("fs")
+const cheerio = require("cheerio")
+
+const directories = [
+  "accounts",
+  "app",
+  "check-ins",
+  "giving",
+  "groups",
+  "interfaces",
+  "people",
+  "planning-center",
+  "registrations",
+  "services",
+]
+
+directories.forEach(dir => {
+  const svgNames = fs.readdirSync(dir).filter(x => x !== ".DS_Store");
+
+  svgNames.forEach(filename => {
+    const file = fs.readFileSync(`${dir}/${filename}`, "utf8")
+
+    const svg = cheerio.load(file)
+
+    svg("svg").attr("aria-labelledby", "title")
+
+    fs.writeFileSync(`./${dir}/${filename}`, svg.html())
+  })
+})
