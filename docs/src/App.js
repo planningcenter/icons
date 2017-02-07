c04a41723f6026bc385c489f434223b8673be774
Michael Chan <mijoch@gmail.com>
2017-02-06 18:36:15 -0800

    feat: show Accounts icons

--- docs/src/App.js
+++ docs/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from '../../planning-center/color-planning-center-mark.svg';
 import './App.css';
 
+import accountsIcons from "./accounts-icons"
+
 class App extends Component {
   render() {
     return (
@@ -16,6 +18,37 @@ class App extends Component {
           />
           <h2>Planning Center Icons</h2>
         </div>
+
+        <h3>Accounts Icons</h3>
+        <table style={{margin: "0 auto"}}>
+        <style>{`th, td { padding: 1rem }`}</style>
+          <thead>
+            <tr>
+              <th>svg</th>
+              <th>name</th>
+              <th>collection</th>
+              <th>path</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {Object.keys(accountsIcons).map((icon, i) =>
+              <tr key={i}>
+                <td>
+                  <img
+                    alt={`${accountsIcons[icon]} icon`}
+                    src={accountsIcons[icon]}
+                    height={32}
+                    width={32}
+                  />
+                </td>
+                <td>{icon}</td>
+                <td>accounts</td>
+                <td>account/{icon}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
       </div>
     );
   }
