0e60ceb88bef78b44f99794b3f4db8807aaea29e
Michael Chan <mijoch@gmail.com>
2017-02-22 16:43:37 -0800

    docs: add components with links

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import logo from '../../svgs/planning-center/color-planning-center-mark.svg';
 import './App.css';
+const camelCase = require("lodash.camelcase")
+const upperFirst = require("lodash.upperfirst")
 
 const iconSets = [
-  {name: "accounts",        icons: require("./accounts-icons").default},
-  {name: "app",             icons: require("./app-icons").default},
-  {name: "check-ins",       icons: require("./check-ins-icons").default},
-  {name: "giving",          icons: require("./giving-icons").default},
-  {name: "groups",          icons: require("./groups-icons").default},
-  {name: "interfaces",      icons: require("./interfaces-icons").default},
-  {name: "people",          icons: require("./people-icons").default},
-  {name: "planning-center", icons: require("./planning-center-icons").default},
-  {name: "registrations",   icons: require("./registrations-icons").default},
-  {name: "services",        icons: require("./services-icons").default},
+  {name: "accounts",        components: true,  icons: require("./accounts-icons").default},
+  {name: "apps",            components: true,  icons: require("./apps-icons").default},
+  {name: "check-ins",       components: true,  icons: require("./check-ins-icons").default},
+  {name: "color-apps",      components: false, icons: require("./color-apps-icons").default},
+  {name: "giving",          components: true,  icons: require("./giving-icons").default},
+  {name: "groups",          components: true,  icons: require("./groups-icons").default},
+  {name: "interfaces",      components: true,  icons: require("./interfaces-icons").default},
+  {name: "people",          components: true,  icons: require("./people-icons").default},
+  {name: "planning-center", components: false, icons: require("./planning-center-icons").default},
+  {name: "registrations",   components: true,  icons: require("./registrations-icons").default},
+  {name: "services",        components: true,  icons: require("./services-icons").default},
 ]
+
+const getComponentName = name => `${upperFirst(camelCase(name))}Icon`
 
 class App extends Component {
   render() {
@@ -30,7 +35,7 @@ class App extends Component {
           <h2>Planning Center Icons</h2>
         </div>
 
-        {iconSets.map(({icons, name}, i) =>
+        {iconSets.map(({icons, components, name}, i) =>
           <section
             key={i}
             id={name}
@@ -52,6 +57,7 @@ class App extends Component {
                   <th>svg</th>
                   <th>name</th>
                   <th>path</th>
+                  {components && <th>component</th>}
                 </tr>
               </thead>
 
@@ -70,8 +76,18 @@ class App extends Component {
                       <a
                         href={`https://github.com/planningcenter/icons/tree/master/svgs/${name}/${icon}.svg`}
                         target="_blank"
-                      >./{name}/{icon}</a>
+                      >{name}/{icon}</a>
                     </td>
+                    {components &&
+                      <td>
+                        <a
+                          href={`https://github.com/planningcenter/icons/tree/master/components/${name}/${getComponentName(icon)}.js`}
+                          target="_blank"
+                        >
+                          {getComponentName(icon)}
+                        </a>
+                      </td>
+                    }
                   </tr>
                 )}
               </tbody>
