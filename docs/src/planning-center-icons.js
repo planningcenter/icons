dfca6595894bce76dcf443fb0baa9f79a1fffdc6
Michael Chan <mijoch@gmail.com>
2017-02-24 08:41:35 -0800

    docs: remove gh-pages app. sprockets-es6-amd is super eager

--- docs/src/planning-center-icons.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "color-planning-center-badge": require("../../svgs/planning-center/color-planning-center-badge.svg"),
-  "color-planning-center-mark": require("../../svgs/planning-center/color-planning-center-mark.svg"),
-  "light-planning-center-badge": require("../../svgs/planning-center/light-planning-center-badge.svg"),
-  "light-planning-center-mark": require("../../svgs/planning-center/light-planning-center-mark.svg"),
-  "planning-center-badge": require("../../svgs/planning-center/planning-center-badge.svg"),
-  "planning-center-mark": require("../../svgs/planning-center/planning-center-mark.svg"),
-}
