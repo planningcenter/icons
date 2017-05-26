07c7121d19b205f396e8dd6a964eda0f1eceab0f
Jesse J. Anderson <jessejanderson@gmail.com>
2017-05-26 14:55:56 -0700

    update reservation-block icon

--- components/resources/ReservationBlock.js
+++ components/resources/ReservationBlock.js
@@ -18,13 +18,21 @@
     { id: "title" },
     "reservation-block icon"
   ),
-  React.createElement("path", { className: "cls-1", d: "M14 1.5h-1V.75a.75.75 0 0 0-1.5 0v.75h-7V.75a.75.75 0 0 0-1.5 0v.75H2a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2zm.5 12.5a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5V6a.5.5 0 0 1 .5-.5h12a.5.5 0 0 1 .5.5z", role: "presentation" }),
-  React.createElement("rect", { className: "cls-1", x: "2.5", y: "6.5", width: "3", height: "3", rx: ".5", ry: ".5" }),
-  React.createElement("rect", { className: "cls-1", x: "10.5", y: "6.5", width: "3", height: "3", rx: ".5", ry: ".5" }),
-  React.createElement("rect", { className: "cls-1", x: "6.5", y: "6.5", width: "3", height: "3", rx: ".5", ry: ".5" }),
-  React.createElement("rect", { className: "cls-1", x: "2.5", y: "10.5", width: "3", height: "3", rx: ".5", ry: ".5" }),
-  React.createElement("rect", { className: "cls-1", x: "10.5", y: "10.5", width: "3", height: "3", rx: ".5", ry: ".5" }),
-  React.createElement("rect", { className: "cls-1", x: "6.5", y: "10.5", width: "3", height: "3", rx: ".5", ry: ".5" })
+  React.createElement(
+    "g",
+    { "data-name": "Reservation Block" },
+    React.createElement("path", { "data-name": "path0 fill", className: "cls-1", d: "M12.25 0a.75.75 0 0 0-.75.75v.75h-7V.75a.75.75 0 0 0-1.5 0v.75H2a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1V.75a.75.75 0 0 0-.75-.75zM2 14.5a.5.5 0 0 1-.5-.5V6a.5.5 0 0 1 .5-.5h12a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-.5.5z", role: "presentation" }),
+    React.createElement(
+      "g",
+      { "data-name": "Rectangle 5" },
+      React.createElement("path", { "data-name": "path1 fill", className: "cls-1", d: "M9 7H3.5a.5.5 0 0 0-.5.5v1.25a.5.5 0 0 0 .5.5H9a.5.5 0 0 0 .5-.5V7.5A.5.5 0 0 0 9 7z", role: "presentation" })
+    ),
+    React.createElement(
+      "g",
+      { "data-name": "Rectangle 5" },
+      React.createElement("path", { "data-name": "path1 fill", className: "cls-1", d: "M12.5 10.75H7a.5.5 0 0 0-.5.5v1.25a.5.5 0 0 0 .5.5h5.5a.5.5 0 0 0 .5-.5v-1.25a.5.5 0 0 0-.5-.5z", role: "presentation" })
+    )
+  )
 );;
   }
 
