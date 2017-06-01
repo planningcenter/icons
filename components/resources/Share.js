8e7bd2e061f70de0cfa0ffda41a96f1b2f4405c1
Jesse J. Anderson <jessejanderson@gmail.com>
2017-05-31 18:31:56 -0700

    update minor tweaks

--- components/resources/Share.js
+++ components/resources/Share.js
@@ -19,7 +19,7 @@
     "share icon"
   ),
   React.createElement("path", { "data-name": "path0 fill", className: "cls-1", d: "M7.47.22a.75.75 0 0 1 1.06 0l2.5 2.5a.75.75 0 0 1-1.06 1.06L8.75 2.561v7.689a.75.75 0 0 1-1.5 0V2.561L6.03 3.78a.75.75 0 0 1-1.06-1.06z", role: "presentation" }),
-  React.createElement("path", { "data-name": "path1 fill", className: "cls-1", d: "M4.5 6.5A.5.5 0 0 0 4 7v7a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V7a.5.5 0 0 0-.5-.5H11A.75.75 0 1 1 11 5h.5a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2h-7a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2H5a.75.75 0 0 1 0 1.5z", role: "presentation" })
+  React.createElement("path", { "data-name": "path1 fill", className: "cls-1", d: "M4.5 6.5A.5.5 0 0 0 4 7v7a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V7a.5.5 0 0 0-.5-.5H11A.75.75 0 0 1 11 5h.5a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2h-7a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2H5a.75.75 0 0 1 0 1.5z", role: "presentation" })
 );;
   }
 
