e112d223c7635e8bea0d5207fbfed9940b10b807
Travis Arnold <ftntravis@gmail.com>
2020-10-09 13:06:52 -0700

    feat: add figma script to source svgs

---
+++ scripts/figma.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+
+const { fetchImages } = require('figma-tools')
+const { writeFileSync } = require('fs')
+const { resolve } = require('path')
+const SVGO = require('svgo')
+
+const svgo = new SVGO({
+  plugins: [
+    { removeUselessStrokeAndFill: true },
+    { convertShapeToPath: true },
+    { removeDimensions: true },
+    { removeAttrs: { attrs: '(stroke|fill)' } },
+  ],
+})
+
+fetchImages({
+  fileId: 'jkzIOE4y4uwlpUZZc6z4Nz',
+  format: 'svg',
+}).then(async (svgs) => {
+  if (svgs.length === 0) {
+    console.log(
+      'No components found. Please make sure there are components in the file to export.'
+    )
+  } else {
+    const optimizedSvgs = await Promise.all(
+      svgs.map((svg) =>
+        svgo.optimize(svg.buffer.toString()).catch((err) => {
+          console.error(err)
+          console.log(svg)
+        })
+      )
+    )
+    svgs.forEach((svg, index) => {
+      const optimizedSvg = optimizedSvgs[index].data
+      writeFileSync(
+        resolve(
+          __dirname,
+          `../svg/${svg.frameName.toLowerCase()}/${svg.name}.svg`
+        ),
+        optimizedSvg
+      )
+    })
+  }
+})
